425f5f4f8052127d8cbdaad027d838aa2b53a86c
Yingshi Liu <serendipper.liu@gmail.com>
2021-05-18 13:22:02 -0700

    fix websocket connnection problem

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const express = require('express')
 
 const app = express()
 
-
 const port = process.env.PORT || 3000
 
 // Define path for Express Config
@@ -14,19 +13,10 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Use express meddleware to serve public content
 app.use(express.static(publicDirectoryPath))
 
-
-/**
- * Console which port server is listening to
- */
-const server = app.listen(port, () => {
-    console.log(`Server is up on ${port} now!`)
-})
-// const server = http.createServer(app)
-socketio.listen(server)
-
-// Setup Socket.io to work with raw http server
+const server = http.createServer(app)
+ 
 const io = socketio(server)
-// Set up message when socket.io gets the below message
+ 
 io.on('connection', (socket) => {
     console.log('New web socket connection')
     socket.emit("Welcome Message", "Welcome!")
@@ -34,3 +24,5 @@ io.on('connection', (socket) => {
         io.emit('Message', message)
     })
 })
+ 
+server.listen(3000)
